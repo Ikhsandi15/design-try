052951e7453a5ca5711187a017705b762824d7f6
Jantur-dev <jantur29@gmail.com>
2023-09-14 22:31:44 +0700

    updated on 14/09/2023

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "780px": "48.75rem",
         "1083px": "1083px",
         "36%": "36%",
+        "32%": "32%",
         "80px": "80px"
       },
       "spacing": {
@@ -14,7 +15,9 @@ module.exports = {
       },
       height: {
         '861px': '861px',
-        '900px': '890px'
+        '900px': '900px',
+        '990px': '990px',
+        '800px': '800px'
       },
       backgroundImage: {
         'diagonal-grid': "url('../src/img/diagonal-grid-trans5.png')",
@@ -22,7 +25,8 @@ module.exports = {
         'hero-section': "url('../src/img/hero.jpg')",
         'pcb-background': "url('../src/img/pcbbg.png')",
         "bg-shape1": "url('../src/img/bg-shape1.jpg')",
-        "bg-shape2": "url('../src/img/bg-shape2.jpg')"
+        "bg-shape2": "url('../src/img/bg-shape2.jpg')",
+        'bg-wave1': 'url(../src/img/bg-wave1.jpg)'
       }
     },
   },
